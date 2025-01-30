78dc7669f70ece9623b160bdc7c57ca60bd25c48
Ajit sharma <ajitsharma1006@gmail.com>
2025-01-30 22:30:30 +0530

    new assignment

---
+++ C -DSA/Average.c
@@ -0,0 +1,2 @@
+////Q1 in a program , input 3 number : A,B and C . you have to output the average of these 3 numbers
+#include<stdio.h>
---
+++ output.java
@@ -0,0 +1,14 @@
+// What will be the output of following programs:-
+
+public  class  output {
+    public static void main(String[] args) {
+        int x=2, y=5;
+        int exp1 = (x*y/x);
+        int exp2 = (x*(y/x));
+        System.err.println("exp1 :- "+exp1);
+        System.out.println("exp2:- "+exp2);
+    }
+}
+
+// exp1 :- 5
+// exp2:- 4
